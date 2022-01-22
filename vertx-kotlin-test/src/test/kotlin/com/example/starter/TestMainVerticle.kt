58f52aff8e9366eb9e652c6da61f2e44b9324908
gronblom <gronblom@gmail.com>
2022-01-22 22:30:08 +0200

    Add basic vertx kotlin unit test

--- vertx-kotlin-test/src/test/kotlin/com/example/starter/TestMainVerticle.kt
+++ vertx-kotlin-test/src/test/kotlin/com/example/starter/TestMainVerticle.kt
@@ -1,11 +1,24 @@
 package com.example.starter
 
+import com.fasterxml.jackson.databind.util.ClassUtil
 import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import io.vertx.core.http.HttpClientRequest
+import io.vertx.core.http.HttpClientResponse
+import io.vertx.core.http.HttpMethod
+import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
+import io.vertx.core.parsetools.JsonParser
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
+import io.vertx.kotlin.coroutines.await
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
+import java.util.function.Function
+
 
 @ExtendWith(VertxExtension::class)
 class TestMainVerticle {
@@ -17,6 +30,22 @@ class TestMainVerticle {
 
   @Test
   fun verticle_deployed(vertx: Vertx, testContext: VertxTestContext) {
-    testContext.completeNow()
+    val httpClient = vertx.createHttpClient()
+    httpClient.request(HttpMethod.GET, 8888, "localhost", "")
+      .compose(HttpClientRequest::send)
+      .compose(HttpClientResponse::body)
+      .onComplete(testContext.succeeding { buffer ->
+        val responseJson: JsonObject = buffer.toJsonObject()
+        println(responseJson)
+        Assertions.assertTrue(responseJson.containsKey("jsonIsValid"))
+        Assertions.assertFalse(responseJson.getBoolean("jsonIsValid")) // No JSON body yet
+        Assertions.assertTrue(responseJson.containsKey("jwt"))
+        //
+        val jwtParts = responseJson.getString("jwt").split(".")
+        val jwtPayload = JsonObject(String(Base64.getDecoder().decode(jwtParts[1])))
+        Assertions.assertTrue(jwtPayload.containsKey("foo"))
+        Assertions.assertTrue(jwtPayload.getString("foo").equals("bar"))
+        testContext.completeNow()
+      })
   }
 }
